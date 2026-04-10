748361d0882f7009ef297274c7007a8630d29530
Franco Polizzi <me@fpolizzi.com>
2026-04-10 23:10:25 +0200

    test: add various test for remaining requirements

--- src/test/java/com/fpolizzi/PasswordValidatorTest.java
+++ src/test/java/com/fpolizzi/PasswordValidatorTest.java
@@ -1,6 +1,8 @@
 package com.fpolizzi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -49,5 +51,78 @@ class PasswordValidatorTest {
 
         // Then
         assertThat(actual).isFalse();
+    }
+
+    @Test
+    void willFailIfPasswordDoesNotMeetsMinimumLengthRequiredAfterTrimming() {
+
+        // Given
+        String password = "  pass  ";
+
+        // When
+        var actual = underTest.test(password);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void willFailIfPasswordDoesNotContainsAtLeastOneDigit() {
+
+        // Given
+        String password = "password";
+
+        // When
+        var actual = underTest.test(password);
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void willFailIfPasswordDoesNotContainsAtLeastOneSpecialChar() {
+
+        // Given
+        String password = "password1";
+
+        // When
+        var actual = underTest.test(password);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Password1!, true",
+            "Password1@, true",
+            "Password1#, true",
+            "Password1$, true",
+            "Password1%, true",
+            "Password1^, true",
+            "Password1&, true",
+            "Password1*, true",
+            "Password1(, true",
+            "Password1), true",
+            "Password1_, true",
+            "Password1+, true",
+            "Password1=, true",
+            "Password1<, true",
+            "Password1>, true",
+            "Password1?, true",
+            "Password1/, true",
+            "Password1[, true",
+            "Password1], true",
+            "Password1{, true",
+            "Password1}, true",
+            "Password1|, true",
+            "Password1€, false",
+    })
+    void canValidateEspecialCharacters(String password,
+                                       boolean expected) {
+        // When
+        var actual = underTest.test(password);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
     }
 }
