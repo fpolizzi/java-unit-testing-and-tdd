09a67fe3d188b9619401944b34968cff35e6e316
Franco Polizzi <me@fpolizzi.com>
2026-04-10 23:13:26 +0200

    feat: add remaining requirements driven by the various test cases

--- src/test/java/com/fpolizzi/PasswordValidator.java
+++ src/test/java/com/fpolizzi/PasswordValidator.java
@@ -7,13 +7,28 @@ import java.util.function.Predicate;
  */
 public class PasswordValidator implements Predicate<String> {
 
+    public static final int MIN_PASSWORD_LENGTH = 8;
+    private static final String DIGIT_REGEX = ".*\\d.*";
+    private static final String SPECIAL_CHAR_REGEX = ".*[!@#$%^&*()_+=<>?/\\[\\]{}|].*";
+
     @Override
     public boolean test(String password) {
 
-        if (password == null || password.isBlank()) {
+        if (password == null || password.trim().isBlank()) {
             return false;
         }
 
-        return false;
+        password = password.trim();
+
+        if (password.length() < MIN_PASSWORD_LENGTH) {
+
+            return false;
+        }
+
+        if(!password.matches(DIGIT_REGEX)) {
+            return false;
+        }
+
+        return password.matches(SPECIAL_CHAR_REGEX);
     }
 }
