e20002d2336a171208382f09196c1a2f65730896
Franco Polizzi <me@fpolizzi.com>
2026-04-10 22:27:33 +0200

    test: add willFailIfPasswordIsNull

---
+++ src/test/java/com/fpolizzi/PasswordValidatorTest.java
@@ -0,0 +1,14 @@
+package com.fpolizzi;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by fpolizzi on 10.04.26
+ */
+class PasswordValidatorTest {
+
+    @Test
+    void willFailIfPasswordIsNull() {
+
+    }
+}
