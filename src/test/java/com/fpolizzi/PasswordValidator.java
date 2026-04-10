4716a1c06d69ced001d2d0c02947008dcdab1e3d
Franco Polizzi <me@fpolizzi.com>
2026-04-10 22:40:15 +0200

    feat: add class under test PasswordValidator

---
+++ src/test/java/com/fpolizzi/PasswordValidator.java
@@ -0,0 +1,14 @@
+package com.fpolizzi;
+
+import java.util.function.Predicate;
+
+/**
+ * Created by fpolizzi on 10.04.26
+ */
+public class PasswordValidator implements Predicate<String> {
+
+    @Override
+    public boolean test(String password) {
+        return false;
+    }
+}
