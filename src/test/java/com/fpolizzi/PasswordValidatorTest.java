903df74041fcf88bf6aea5e6a9d01f80810b7d6f
Franco Polizzi <me@fpolizzi.com>
2026-04-10 22:34:06 +0200

    test: add how the test should work

--- src/test/java/com/fpolizzi/PasswordValidatorTest.java
+++ src/test/java/com/fpolizzi/PasswordValidatorTest.java
@@ -2,13 +2,26 @@ package com.fpolizzi;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by fpolizzi on 10.04.26
  */
 class PasswordValidatorTest {
 
+    private final PasswordValidator underTest =
+            new PasswordValidator();
+
     @Test
     void willFailIfPasswordIsNull() {
 
+        // Given
+        String password = null;
+
+        // When
+        var actual = underTest.test(password);
+
+        // Then
+        assertThat(actual).isFalse();
     }
 }
