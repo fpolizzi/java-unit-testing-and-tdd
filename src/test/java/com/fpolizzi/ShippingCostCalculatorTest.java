872e7d212635b7512d65a1bbf1c8a9545535d1e6
Franco Polizzi <me@fpolizzi.com>
2026-04-11 15:13:48 +0200

    test: add ShoppingCostCalculatorTest

---
+++ src/test/java/com/fpolizzi/ShippingCostCalculatorTest.java
@@ -0,0 +1,13 @@
+package com.fpolizzi;
+
+/**
+ * Created by fpolizzi on 11.04.26
+ */
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ShippingCostCalculatorTest {
+
+    private final ShippingCostCalculator underTest = new ShippingCostCalculator();
+
+}
