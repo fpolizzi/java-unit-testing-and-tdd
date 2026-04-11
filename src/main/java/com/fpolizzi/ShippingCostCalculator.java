05ffc76b5008cd20c1dc1acb75c23610b53aa393
Franco Polizzi <me@fpolizzi.com>
2026-04-11 15:13:20 +0200

    feat: add ShoppingCostCalculator

---
+++ src/main/java/com/fpolizzi/ShippingCostCalculator.java
@@ -0,0 +1,12 @@
+package com.fpolizzi;
+
+/**
+ * Created by fpolizzi on 11.04.26
+ */
+public class ShippingCostCalculator {
+
+    public double calculate(double weightKg, String region, boolean express) {
+        // TODO: implement using TDD
+        return 0.0;
+    }
+}
