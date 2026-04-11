568a3eea2598ebc05ae6896d06735e37da2802eb
Franco Polizzi <me@fpolizzi.com>
2026-04-12 00:30:23 +0200

    feat: add solution for fourth testcase

--- src/main/java/com/fpolizzi/ShippingCostCalculator.java
+++ src/main/java/com/fpolizzi/ShippingCostCalculator.java
@@ -7,14 +7,16 @@ public class ShippingCostCalculator {
 
     public double calculate(double weightKg, String region, boolean express) {
         // TODO: implement using TDD
-        double base = 0;
+        double base;
 
         if (weightKg <= 1.0 ) {
             base = 5.00;
         } else if (weightKg <= 5) {
             base = region.equalsIgnoreCase("Local") ? 10.00 : 20.00;
+        } else {
+            base = region.equalsIgnoreCase("Local") ? 15.00 : 30.00;
         }
 
-        return base;
+        return express ? base + 15.00 : base;
     }
 }
